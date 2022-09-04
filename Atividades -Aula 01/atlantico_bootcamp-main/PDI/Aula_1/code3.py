ef1ba75cd45610b46780147835f6e380cbf64e90
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-09-04 10:47:29 -0300

    Atividades aula 01

--- Atividades -Aula 01/atlantico_bootcamp-main/PDI/Aula_1/code3.py
+++ Atividades -Aula 01/atlantico_bootcamp-main/PDI/Aula_1/code3.py
@@ -3,20 +3,22 @@ import matplotlib.pyplot as plt
 
 from PDI.src.pdi_utils import load_red_roses,show_image
 import matplotlib.pyplot as plt
+import cv2
 
 image = load_red_roses()
 
 # Show original image
-__(__,'image RGB')
+show_image(image, 'image RGB')
 
 # Obtain the red channel
-red_channel = __
+# (blue_red, green_chanel, red_channel) = cv2.split(image)
+red_channel = image[:, :, 0]
 
 # Show original image
-__(__,'image red channel')
+show_image(red_channel, 'image red channel')
 
 # Plot the red histogram with bins in a range of 256
-plt.__(__, bins=__)
+plt.hist(red_channel, bins=256)
 
 # Set title and show
 plt.title('Red Histogram')
